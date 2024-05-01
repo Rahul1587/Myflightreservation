c76a397f5d00b905509d033985d7b90999282f44
Rahul1587 <86835631+Rahul1587@users.noreply.github.com>
2024-05-01 18:20:34 +0530

    Delete flightreservation directory

--- flightreservation/repos/ReservationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rahul.flightreservation.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rahul.flightreservation.entities.Reservation;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-
-}
--- flightreservation/services/ReservationService.java
+++
@@ -1,9 +0,0 @@
-package com.rahul.flightreservation.services;
-
-import com.rahul.flightreservation.dto.ReservationRequest;
-import com.rahul.flightreservation.entities.Reservation;
-
-public interface ReservationService {
-
-	Reservation bookFlight(ReservationRequest request);
-}
